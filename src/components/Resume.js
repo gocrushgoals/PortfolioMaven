70502da3d7a118bfbdedd94adadf674041665df9
gocrushgoals <gocrushgoals@gmail.com>
2024-06-22 18:40:00 -0400

    add resume and tech skills section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,8 +11,6 @@ const Resume = () => (
       <li>JavaScript</li>
       <li>React</li>
       <li>Node.js</li>
-      <li>HTML</li>
-      <li>CSS</li>
       <li>MongoDB</li>
       <li>MySQL</li>
       <li>Express</li>
