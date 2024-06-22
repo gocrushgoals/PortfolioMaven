ff87f4a6daf178d3d75d952dc8cb252e995a7917
gocrushgoals <gocrushgoals@gmail.com>
2024-06-22 17:47:01 -0400

    add footer template

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => (
+  
+  <footer>
+    <p>
+      <a href="https://www.linkedin.com/">
+         LinkedIn
+      </a>
+      <a href="https://github.com/">
+         Github
+      </a>
+      <a href="https://stacoverflow.com/">
+         Stack Overflow
+      </a>
+    </p>
+  </footer>
+);
