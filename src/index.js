8249536d1d70c57eb633c4b1e7edc88c500e23a2
gocrushgoals <gocrushgoals@gmail.com>
2024-06-22 19:49:38 -0400

    add index.html page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import './App.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render();
