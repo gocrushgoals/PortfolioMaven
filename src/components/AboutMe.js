d63f945e917ebe881cb790c7b10ad3836348e337
gocrushgoals <gocrushgoals@gmail.com>
2024-06-22 17:51:41 -0400

    add about me section template

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => (
+  <section id="about-me">
+    <h2>About Me</h2>
+    <img src="../assets/images/maven.jpg" alt="profile picture" />"
+    <p>
+      Hi, my name is... my name is... my name is...
+    </p>
+    </section>
+);
+
+export default AboutMe;
