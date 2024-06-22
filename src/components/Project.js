60b854dfc746f5431adec105660e7ccfce884375
gocrushgoals <gocrushgoals@gmail.com>
2024-06-22 17:40:47 -0400

    add component to display individual projects

---
+++ src/components/Project.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Project = ({ Project }) => (
+  <div className="project">
+    <img src={project.image} alt={project.title} />
+    <h3>{project.title}</h3>
+    <p>
+      <a href={project.liveURL}>Live</a> | <a href={project.repoLink}>GitHub</a>
+    </p>
+  </div>
+);
+
+export default Project;
