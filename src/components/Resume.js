2e10f6ff44658bc27ad3764a14da406098517989
gocrushgoals <gocrushgoals@gmail.com>
2024-06-22 18:37:36 -0400

    resume section added

---
+++ src/components/Resume.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Resume = () => (
+  <section id="resume">
+    <h2>Resume</h2>
+    <a href="path/to/resume.pdf" download>
+      Download Resume
+    </a>
+    <h3>Proficiencies</h3>
+    <ul>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Node.js</li>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>MongoDB</li>
+      <li>MySQL</li>
+      <li>Express</li>
+    </ul>
+  </section>
+);
+
+export default Resume;
