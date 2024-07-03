8dce35ad67f9cf8682de2478b9087cb7b8918f63
gocrushgoals <gocrushgoals@gmail.com>
2024-07-03 14:36:59 -0400

    update portfolio.js to handle props and image paths better

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,25 +2,32 @@ import React from 'react';
 import Project from './Project';
 import '../App.css';
 
-
 const projects = [
   {
-    //image: "projectImagePath.jpg",
+    image: "/assets/images/maven.jpg.png",
     title: "Project Title",
     description: "Project Description",
     link: "https://www.google.com"
   },
-  //add more projects here
+  {
+    image: "/assets/images/maven.jpg.png",
+    title: "Project 2 Title",
+    description: "Project 2 Description",
+    link: "https://www.google.com"
+  }
+  // Add more projects here
 ];
 
 const Portfolio = () => (
   <section id="portfolio">
+    <h3>Portfolio</h3>
     <div className="projects">
       {projects.map((project) => (
-        <Project
-          key={project.title}
+        <Project 
+          key={project.title} 
           title={project.title}
           description={project.description}
+          image={project.image}
           link={project.link}
         />
       ))}
