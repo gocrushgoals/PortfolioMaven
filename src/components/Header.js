42092cc6cd1d4b1fde7a723d79005c6c0f85ab5c
gocrushgoals <gocrushgoals@gmail.com>
2024-07-03 14:15:32 -0400

    updated head.js to inclue navLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,15 @@ import '../App.css';
 
 const Header = () => (
   <header>
-    <h1>Kristi</h1>
-    <Navigation />
+    <h1>Your Name</h1>
+    <nav>
+      <ul>
+        <li><NavLink exact to="/" activeClassName="active">About Me</NavLink></li>
+        <li><NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink></li>
+        <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
+        <li><NavLink to="/resume" activeClassName="active">Resume</NavLink></li>
+      </ul>
+    </nav>
   </header>
 );
 
