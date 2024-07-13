e1b0ec2d73dd892745e092de983e210f37a9bb74
gocrushgoals <gocrushgoals@gmail.com>
2024-07-13 18:25:58 -0400

    viteconfig for netlify

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory for production builds
+    sourcemap: true, // Enable sourcemaps for debugging
+  },
   server: {
-    port: 3000,
-    open: true
-  }
-})
+    port: 3000, // Port for development server
+    open: true, // Open the browser automatically when server starts
+  },
+});
