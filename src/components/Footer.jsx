8913fb3701ac049fa56ee981777cf8cb49bbc2bc
gocrushgoals <gocrushgoals@gmail.com>
2024-07-13 19:08:53 -0400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import '../../src/App.css';
+
 const Footer = () => (
-  
-  <footer>
-    <p>
-      <a href="https://www.linkedin.com/">
-         LinkedIn
-      </a>
-      <a href="https://github.com/">
-         Github
-      </a>
-      <a href="https://stackoverflow.com/">
-         Stack Overflow
-      </a>
+  <footer className="footer">
+    <p className="footer-links">
+      <a href="https://www.linkedin.com/">LinkedIn</a>
+      <a href="https://github.com/">GitHub</a>
+      <a href="https://stackoverflow.com/">Stack Overflow</a>
     </p>
   </footer>
 );
