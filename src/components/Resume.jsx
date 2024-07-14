a718aa986a97df9e8eaf2514882e81da34588859
gocrushgoals <gocrushgoals@gmail.com>
2024-07-13 21:43:24 -0400

    add resume link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ import '../../src/App.css';
 const Resume = () => (
   <section id="resume">
     <h2>Resume</h2>
-    <a href="path/to/resume.pdf" download>
+    <a href="/docs/ResumeKR24.pdf" download>
       Download Resume
     </a>
     <h3>Proficiencies</h3>
